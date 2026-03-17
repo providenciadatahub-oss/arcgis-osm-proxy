35909eecb05a755c4fa4d529a47ec0e6cfd788b8
providenciadatahub-oss <providenciadatahub@gmail.com>
2026-03-17 17:17:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Koop = require('@koopjs/koop')
+const nominatim = require('@koopjs/provider-nominatim')
+const express = require('express')
+
+const koop = new Koop()
+// Registra el proveedor de Nominatim
+koop.register(nominatim)
+
+const app = express()
+// Esto es vital para que ArcGIS Online no bloquee la petición
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(koop.server)
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Proxy GIS corriendo en puerto ${port}`))
